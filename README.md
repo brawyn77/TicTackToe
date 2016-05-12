8fcdfbe30c8d4d1fb43aac71a2fb2f2d43d73265
Brad Wynter <brawyn@Brads-Air.gateway>
2016-05-12 21:39:58 +1000

    gameboard works

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 // The first screen to pop up is the setup screen
-// It is the initial screen where you enter the players names and the nos of games you want to players
-// after you have set the parameters you hit the Submit button to take you to the game screen
+// It is the initial screen where you enter the players names and the nos of games you want to play
+// After you have set the parameters you hit the Submit button to take you to the game screen
 // Player 1 plays first with X and then Player 2 has a turn with placing their O
-// Players alternate turns until either one player gets three in a row and one players wins or neither achieve this and it is a tied game
+// Players alternate turns until either player gets three in a row and one players wins or neither achieve this and it is a tied game
 // The scores are tallied and the games reduced by one until all games are played and a winner declared
+
+// The design is simple to avoid taking players away from the riveting gameplay
+// The html provides all the visual elements without presentation
+// The CSS provides visual design to the visual elements
+// The JS code provides the interaction and manages the game players
+ 
--- js/code.js
+++ js/code.js
@@ -27,10 +27,10 @@ function setupVariables() {
 }
 
 //start game function
+// put players names into the game board
+// put games in games to go
+// put in initial scores into Player1, Player2 and Tied games
 function startGame() {
-    // put players names into the game board
-    // put games in games to go
-    // put in initial scores into Player1, Player2 and Tied games
     $('#gToG').text("Games to go: " + nosOfGames);
     $('#p1W').text(player1Name + " wins: " + player1Wins);
     $('#p2W').text(player2Name + " wins: " + player2Wins);
@@ -42,70 +42,76 @@ function startGame() {
 // checks that 9 moves havent been completed
 // check for an existing move in the square
 //if existing then prompt for another move else place image
-// also needs to put them into the array representing the board
-function gameStart()  {
+// also needs to put each move into an array representing the board
+function gameStart() {
 $('.column').on('click', function () {
-    var rowIndex = $(this).parent().index('.row');
-    var columnIndex = $(this).index();
+        var rowIndex = $(this).parent().index('.row');
+        var columnIndex = $(this).index();
 
-    if (ticTacToeArray[rowIndex][columnIndex] == "0") {
+        if (ticTacToeArray[rowIndex][columnIndex] == "0") {
 
-        if (turn % 2 === 1) {
-            $(this).append('<img src="X.png" alt="">')
-                .css('border-color', 'black');
-            ticTacToeArray[rowIndex][columnIndex] = player1Token;
-            console.table(ticTacToeArray);
-            checkSuccess(rowIndex, columnIndex);
-            turn++;
-          } else {
-            $(this).append('<img src="O.png" alt="">')
-                .css('border-color', 'black');
-            ticTacToeArray[rowIndex][columnIndex] = player2Token;
-            console.table(ticTacToeArray);
-            checkSuccess(rowIndex, columnIndex);
-            turn++;
-          }
-      } else {
-          console.log("Pick again");
-      }
-  });
+        if (turn % 2 == 1) {
+                $(this).append('<img src="X.png" alt="">')
+                    .css('border-color', 'black');
+                ticTacToeArray[rowIndex][columnIndex] = player1Token;
+// console.table(ticTacToeArray);
+                checkSuccess(rowIndex, columnIndex);
+                turn++;
+            } else {
+                $(this).append('<img src="O.png" alt="">')
+                    .css('border-color', 'black');
+                ticTacToeArray[rowIndex][columnIndex] = player2Token
+// console.table(ticTacToeArray);
+                checkSuccess(rowIndex, columnIndex);
+                turn++;
+            }
+
+        } else {
+            console.log("Pick again");
+        }
+    });
 }
-// function to load winner arrays after each move alternating between X and O
+
 // function to check arrays for a winner after 5th move
-
+// transform the array of three arrays of 3 elements each into a single array with 9 elements
+// develop a winner template for each of the 8 win conditions using the element indexes to pull out the values
+// player 1 uses a value of 1 for each square occupied and player 2 uses a value of 10
+// the tests below look for the sum of a single lines being 3 for X or 30 for O
 function checkSuccess(rowIndex, columnIndex) {
-  var merged = [].concat.apply([], ticTacToeArray);
-  var winner1 = merged[0] + merged[1] + merged[2];
-  var winner2 = merged[3] + merged[4] + merged[5];
-  var winner3 = merged[6] + merged[7] + merged[8];
-  var winner4 = merged[0] + merged[3] + merged[6];
-  var winner5 = merged[1] + merged[4] + merged[7];
-  var winner6 = merged[2] + merged[5] + merged[8];
-  var winner7 = merged[0] + merged[4] + merged[8];
-  var winner8 = merged[6] + merged[5] + merged[2];
-  var winner = [winner1, winner2, winner3, winner4, winner5, winner6, winner7, winner8];
+    var merged1 = [].concat.apply([], ticTacToeArray);
+    var merged2 = [].concat.apply([], ticTacToeArray);
+    var merged3 = [].concat.apply([], ticTacToeArray);
+    var merged4 = [].concat.apply([], ticTacToeArray);
+    var merged5 = [].concat.apply([], ticTacToeArray);
+    var merged6 = [].concat.apply([], ticTacToeArray);
+    var merged7 = [].concat.apply([], ticTacToeArray);
+    var merged8 = [].concat.apply([], ticTacToeArray);
+    var winner1 = merged1[0] + merged1[1] + merged1[2];
+    var winner2 = merged2[3] + merged2[4] + merged2[5];
+    var winner3 = merged3[6] + merged3[7] + merged3[8];
+    var winner4 = merged4[0] + merged4[3] + merged4[6];
+    var winner5 = merged5[1] + merged5[4] + merged5[7];
+    var winner6 = merged6[2] + merged6[5] + merged6[8];
+    var winner7 = merged7[0] + merged7[4] + merged7[8];
+    var winner8 = merged8[2] + merged8[4] + merged8[6];
+    var winner = [winner1, winner2, winner3, winner4, winner5, winner6, winner7, winner8];
 
-  for (var i = 0; i < winner.length; i++) {
-    console.log(winner[i]);
-    if (winner[i] == 3)  {
-      console.log("X wins");
-      runWinner();
-    }
-    else if (winner[i] == 30)  {
-      console.log("O wins")
-      runWinner();
-    }
-    else  {
-      return("no wins")
+    for (var n = 0; n < winner.length; n++) {
+      if (winner[n] == 3)  {
+        win = 3;
+        runWinner(win);
+      }
+      else if (winner[n] == 30)  {
+        win = 5;
+        runWinner(win);
+      }
     }
   }
-}
 
-//winner function
-//scoring and games to go function
+//winner function - scoring and games to go function
 function runWinner(x)  {
-  console.log("run winner")
-  $(this).off('click');
+  console.log("Winner")
+  clickOff();
   if (x = 3) {
     player1Wins++;
     gamesToGo--;
@@ -118,6 +124,20 @@ function runWinner(x)  {
   }
 }
 
+// function to turn off the game function
+function clickOff ()  {
+  $('.column').off("click", gameStart);
+}
+
+// click off the buttons so that only the game play can occur
+function buttonClickOff () {
+  $("button").off("click");
+}
+
+// click on the buttons so that they can be used
+function buttonClickOn ()  {
+  $("button").on("click");
+}
 //reset game function
 function resetValues() {
     player1Name = "";
@@ -134,4 +154,3 @@ function resetValues() {
 // resetValues();
 
 // To dos-: fix update readme.md, put it all together
-// bonus: put in three games Tic Tac Toe, Tic Tacky Toes, Ticky Tacky Election
